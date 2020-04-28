03b8a7ad21a85fd6235d01d58aa97e5b535fd3e2
tslinardo <41757475+tslinardo@users.noreply.github.com>
2020-04-28 16:58:24 -0700

    Added action listener and pop up with names, plus started new version class

--- OrganizationApp/src/gui/MainFrame.java
+++ OrganizationApp/src/gui/MainFrame.java
@@ -2,6 +2,10 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -9,14 +13,16 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
 
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements ActionListener {
 
 	private ContentPanel contentPanel;
 	private SidePanel sidePanel;
 	private JToolBar toolBar;
+    private JMenuItem myAboutMenu;
 	
 	public MainFrame() {
 		
@@ -43,12 +49,34 @@ public class MainFrame extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 		JMenu fileMenu = new JMenu("File");
 		JMenu aboutMenu = new JMenu("About");
+		
 		JMenuItem add = new JMenuItem("add");
 		JMenuItem remove = new JMenuItem("remove");
+		myAboutMenu = new JMenuItem("About Program");
+		myAboutMenu.addActionListener(this);
+		
+		aboutMenu.add(myAboutMenu);
 		fileMenu.add(add);
 		fileMenu.add(remove);
 		menuBar.add(fileMenu);
 		menuBar.add(aboutMenu);
 		return menuBar;
 	}
+    
+	/**
+	 * Creates pop-up message with info when user click about Program tab in About Menu 
+	 * @author Tatiana Linardopoulou
+	 */
+	@Override
+	public void actionPerformed(ActionEvent theEvent) {
+		if (theEvent.getSource().equals(myAboutMenu)) {
+			JOptionPane.showMessageDialog(this, "Developers:\n"
+					+ "Adam Hall\n"
+					+ "Seoungdeok Jeon\n"
+					+ "Tatiana Linardopoulou\n");
+
+		}
+	}
+
+	
 }
