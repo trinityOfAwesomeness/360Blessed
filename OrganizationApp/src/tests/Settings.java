37fbddc7d2865e1d2386421d050c71d0b09dc737
tslinardo <41757475+tslinardo@users.noreply.github.com>
2020-05-23 15:10:55 -0700

    adding a test file for Settings

---
+++ OrganizationApp/src/tests/Settings.java
@@ -0,0 +1,34 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Settings {
+
+	@Test
+	public void testGetUserName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
